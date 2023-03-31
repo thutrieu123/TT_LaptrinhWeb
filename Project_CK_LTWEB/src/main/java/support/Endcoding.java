a3d88c3122a2c0849d0f8a0e6c4b86f88122835f
Phuong <phuongnguyen112002@gmail.com>
2023-03-31 14:36:06 +0700

    Chuc nang ma hoa mat khau khi luu xuong data base

--- Project_CK_LTWEB/src/main/java/support/Endcoding.java
+++ Project_CK_LTWEB/src/main/java/support/Endcoding.java
@@ -1,5 +1,12 @@
 package support;
 
+import java.security.MessageDigest;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+
+
+
+
 public class Endcoding {
 	
 	public String decodeEmail(String email) {
@@ -12,9 +19,24 @@ public class Endcoding {
 		return result;
 	}
 	
-	public String encypt(String s) {
-		String result = "";
+	public static String encrypt(String str) {
+		String salt = "adABDK*@asjdkasfl;sada";
+		String result = null;
+		str += str + salt;
+		
+		try {
+			byte[] dataBytes = str.getBytes("UTF-8");
+			MessageDigest md = MessageDigest.getInstance("SHA-1");
+			result = Base64.encodeBase64String(md.digest(dataBytes));
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
-	
+	public static void main(String[] args) {
+		System.out.println(encrypt("Anhphuong123"));
+	}
 
 }
